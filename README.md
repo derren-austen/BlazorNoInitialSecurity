7c99baccc97ea863dbac1095395681ec636b0ef3
derren-austen <derren@bradpaisley.co.uk>
2021-07-07 15:10:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blazor WebAssembly with no authentication
+
+This document will outline how to take a hosted WebAssembly app, with no authentication, and secure it with Azure AD.
---
+++ add-dev-config-settings.ps1
@@ -0,0 +1,25 @@
+﻿# API app
+
+$sourceFile = '.\AnApiNotPartOfTheBlazorInfrastructure\appsettings.json'
+$destinations = '.\AnApiNotPartOfTheBlazorInfrastructure\appsettings.Development.json'
+
+Foreach ($destination in $destinations)
+{
+    if (-not (test-path $destination))
+    {
+        Copy-Item $sourceFile -Destination $destination
+    }
+}
+
+# WASM host server app
+
+$sourceFile = '.\BlazorHosted\Server\appsettings.json'
+$destinations = '.\BlazorHosted\Server\appsettings.Development.json'
+
+Foreach ($destination in $destinations)
+{
+    if (-not (test-path $destination))
+    {
+        Copy-Item $sourceFile -Destination $destination
+    }
+}
